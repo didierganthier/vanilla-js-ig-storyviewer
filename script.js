2580e064214af8e089b2be6f01c32776d899e9c0
Didier Peran Ganthier <didierganthierperan@gmail.com>
2025-02-28 19:36:18 -0500

    Add stories data structure to script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const stories = [
+    {
+        id: 1,
+        image: 'https://randomuser.me/api/portraits/women/1.jpg',
+        user: 'user1'
+    },
+    {
+        id: 2,
+        image: 'https://randomuser.me/api/portraits/women/2.jpg',
+        user: 'user2'
+    },
+    {
+        id: 3,
+        image: 'https://randomuser.me/api/portraits/women/3.jpg',
+        user: 'user3'
+    },
+    {
+        id: 4,
+        image: 'https://randomuser.me/api/portraits/women/4.jpg',
+        user: 'user4'
+    },
+    {
+        id: 5,
+        image: 'https://randomuser.me/api/portraits/women/5.jpg',
+        user: 'user5'
+    }
+];
